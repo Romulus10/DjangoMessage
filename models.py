1923ff037cd54d32a38a9120a446e77890a88616
Sean Batzel <romulus108@protonmail.com>
2021-10-11 21:11:50 -0400

    Initializing this as a submodule.

---
+++ models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=100)
+    content = models.CharField(max_length=2500)
+
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True, editable=False, related_name='sender')
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipient')
+
+    replied_to = models.ForeignKey(
+        "Message", on_delete=models.CASCADE, blank=True, null=True, editable=False)
+
+    read = models.BooleanField(default=False, editable=False)
+    deleted_by_sender = models.BooleanField(default=False, editable=False)
+    deleted_by_recipient = models.BooleanField(default=False, editable=False)
+
+    timestamp = models.DateTimeField(
+        auto_now=True, editable=False, null=False, blank=False)
+
+    def __unicode__(self):
+        return self.subject
+
+    def __str__(self):
+        return self.subject
