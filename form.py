fd6c292b7197bc9b75ba3b75f236052c41a34216
Sean Batzel <romulus108@protonmail.com>
2022-04-05 16:33:46 -0400

    Major quality and performance improvements, in need of testing.

--- form.py
+++ form.py
@@ -1,7 +1,8 @@
 from django.forms.widgets import Textarea
+from django.forms.models import ModelForm
+
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from django.forms.models import ModelForm
 
 from .models import Message
 
